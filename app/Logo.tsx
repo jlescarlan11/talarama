cb781f4b71c3c1cca41a81146550923b39402207
john lester escarlan <jlescarlan11@gmail.com>
2025-05-21 13:41:21 +0800

    build a navbar

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -1,3 +1,4 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
 import { Mansalva } from "next/font/google";
 import { PiFilmReel } from "react-icons/pi";
 
@@ -5,14 +6,17 @@ const mansalva = Mansalva({ subsets: ["latin"], weight: "400" });
 
 const Logo = () => {
   return (
-    <div className="relative">
-      <span
-        className={`flex items-center relative text-2xl ${mansalva.className} z-10 font-bold`}
-      >
-        <PiFilmReel className=" fill-base size-6  stroke-1" />
-        <span className="tracking-[-0.1em]">Talarama</span>
-      </span>
-    </div>
+    <Button
+      variant="ghost"
+      className="!bg-transparent hover:!bg-transparent !cursor-pointer"
+    >
+      <Flex align="center" className="text-2xl px-4 py-2 ">
+        <PiFilmReel />
+        <Text className={`${mansalva.className} !tracking-tighter`}>
+          Talarama
+        </Text>
+      </Flex>
+    </Button>
   );
 };
 
--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,14 @@
-import { Avatar, Flex } from "@radix-ui/themes";
+"use client";
+import { Avatar, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { PiFilmSlate, PiHouse, PiNotebook } from "react-icons/pi";
 import Logo from "./Logo";
 import ThemeToggle from "./ThemeToggle";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { icon: <PiHouse />, label: `Dashboard`, href: "/" },
     { icon: <PiNotebook />, label: "Diary", href: "/diary" },
@@ -18,26 +22,26 @@ const NavBar = () => {
           <Logo />
         </Link>
 
-        <Flex className="items-center" gap="4">
-          <ul className="flex  ">
+        <Flex className="items-center" gap="6">
+          <ul className="flex space-x-2 ">
             {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className=" px-4 py-2 flex rounded-full items-center gap-2"
-              >
-                {link.label}
+              <Link key={link.href} href={link.href}>
+                <Button variant={link.href === currentPath ? "solid" : "soft"}>
+                  {link.label}
+                </Button>
               </Link>
             ))}
           </ul>
-          <Avatar
-            src="https://images.usplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-            radius="full"
-            size="2"
-            fallback="A"
-          />
+          <Flex>
+            <Avatar
+              src="https://images.usplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+              radius="full"
+              size="2"
+              fallback="A"
+            />
 
-          <ThemeToggle />
+            <ThemeToggle />
+          </Flex>
         </Flex>
       </nav>
     </div>
