ecfdabfee2b64ece9a1a6cc11ec3c77500cec7d5
john lester escarlan <jlescarlan11@gmail.com>
2025-05-23 23:49:51 +0800

    added a register form

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,19 +1,51 @@
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+
+        return passwordsMatch ? user : null;
+      },
     }),
   ],
   session: {
     strategy: "jwt",
   },
+  // pages: {
+  //   // signIn: "/auth/signin",
+  //   // signOut: "/auth/signout",
+  //   // error: "/auth/error",
+  //   // verifyRequest: "/auth/verify-request",
+  //   // newUser: "/auth/new-user",
+  // },
 };
 
 export default authOptions;
