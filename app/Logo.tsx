73d97d7bc518bc770260a3ad53022db2c09be8cb
john lester escarlan <jlescarlan11@gmail.com>
2025-06-04 21:12:58 +0800

    fix hydration

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import AppLogo from "../public/appLogo.png";
-import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center mt-2">
+    <div className="flex items-center mt-2">
       <Image src={AppLogo} alt="App Logo" width={200} height={100} />
-    </Link>
+    </div>
   );
 };
 
