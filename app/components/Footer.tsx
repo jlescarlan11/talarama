a9d542ffa1f03204450e028604b3c0ed110e0c7e
john lester escarlan <jlescarlan11@gmail.com>
2025-06-06 08:25:48 +0800

    last push

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 mt-auto bg-gray-100">
-      <div className="container mx-auto px-4 text-center text-gray-600">
-        <p>&copy; 2025 Talarama. A CMSC 127 Project.</p>
-        <p>Built by Bisuela, Caringal, Dela Cruz, and Escarlan</p>
+    <footer className="w-full py-4 mt-auto bg-base-200 text-base-content">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-neutral-content">
+          &copy; 2025 Talarama. A CMSC 127 Project.
+        </p>
+        <p className="text-secondary-content">
+          Built by Bisuela, Caringal, Dela Cruz, and Escarlan
+        </p>
       </div>
     </footer>
   );
